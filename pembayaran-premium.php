9cb41c56d6bf6fddb0c6e57aba244129844964c8
Naufal Luthfi Malik <92344064+Novalllll@users.noreply.github.com>
2022-01-23 18:40:21 +0700

    Back-end Pembayaran premium rev

--- pembayaran-premium.php
+++ pembayaran-premium.php
@@ -210,18 +210,68 @@
     margin-left: 250px;
     margin-right: 110px;
 }
+/*the container must be positioned relative:*/
+.custom-select {
+    position: relative;
+    font-family: 'Poppins', sans-serif;
+}
+.custom-select select {
+    display: none; /*hide original SELECT element:*/
+}
 
-  
-    </style>
+.select-selected {
+    background-color: #233863;
+}
 
-    <!-- Font -->
-    <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link
-      href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
-      rel="stylesheet"
-    />
-    <title>Pembayaran</title>
+/*style the arrow inside the select element:*/
+.select-selected:after {
+    position: absolute;
+    content: "";
+    top: 14px;
+    right: 10px;
+    width: 0;
+    height: 0;
+    border: 6px solid transparent;
+    border-color: #fff transparent transparent transparent;
+}
+
+/*point the arrow upwards when the select box is open (active):*/
+.select-selected.select-arrow-active:after {
+    border-color: transparent transparent #fff transparent;
+    top: 7px;
+}
+
+/*style the items (options), including the selected item:*/
+.select-items div,.select-selected {
+    color: #ffffff;
+    padding: 8px 16px;
+    border: 1px solid transparent;
+    border-color: transparent transparent rgba(0, 0, 0, 0.1) transparent;
+    cursor: pointer;
+    user-select: none;
+}
+
+/*style items (options):*/
+.select-items {
+    position: absolute;
+    background-color: DodgerBlue;
+    top: 100%;
+    left: 0;
+    right: 0;
+    z-index: 99;
+}
+
+/*hide the items when the select box is closed:*/
+.select-hide {
+    display: none;
+}
+
+.select-items div:hover, .same-as-selected {
+    background-color: rgba(0, 0, 0, 0.1);
+}  
+</style>
+
+<title>Pembayaran</title>
 </head>
 <body>
      <!-- navbar -->
@@ -276,72 +326,6 @@
                         <h1>RINCIAN PEMBAYARAN</h1>
                       </div>
                       <div class="payment-method">
-                      <style>
-                        /*the container must be positioned relative:*/
-                        .custom-select {
-                          position: relative;
-                          font-family: 'Poppins', sans-serif;
-                        }
-
-                        .custom-select select {
-                          display: none; /*hide original SELECT element:*/
-                        }
-
-                        .select-selected {
-                          background-color: #233863;
-                        }
-
-                        /*style the arrow inside the select element:*/
-                        .select-selected:after {
-                          position: absolute;
-                          content: "";
-                          top: 14px;
-                          right: 10px;
-                          width: 0;
-                          height: 0;
-                          border: 6px solid transparent;
-                          border-color: #fff transparent transparent transparent;
-                        }
-
-                        /*point the arrow upwards when the select box is open (active):*/
-                        .select-selected.select-arrow-active:after {
-                          border-color: transparent transparent #fff transparent;
-                          top: 7px;
-                        }
-
-                        /*style the items (options), including the selected item:*/
-                        .select-items div,.select-selected {
-                          color: #ffffff;
-                          padding: 8px 16px;
-                          border: 1px solid transparent;
-                          border-color: transparent transparent rgba(0, 0, 0, 0.1) transparent;
-                          cursor: pointer;
-                          user-select: none;
-                        }
-
-                        /*style items (options):*/
-                        .select-items {
-                          position: absolute;
-                          background-color: DodgerBlue;
-                          top: 100%;
-                          left: 0;
-                          right: 0;
-                          z-index: 99;
-                        }
-
-                        /*hide the items when the select box is closed:*/
-                        .select-hide {
-                          display: none;
-                        }
-
-                        .select-items div:hover, .same-as-selected {
-                          background-color: rgba(0, 0, 0, 0.1);
-                        }
-                        </style>
-                        </head>     
-
-                        <body>
-
                         <h2>Metode Pembayaran</h2>
 
                         <!--surround the select box with a "custom-select" DIV element. Remember to set the width:-->
@@ -354,88 +338,87 @@
                           </select>
                         </div>
 
-                          <script>
-                            var x, i, j, l, ll, selElmnt, a, b, c;
-                            /*look for any elements with the class "custom-select":*/
-                            x = document.getElementsByClassName("custom-select");
-                            l = x.length;
-                            for (i = 0; i < l; i++) {
-                              selElmnt = x[i].getElementsByTagName("select")[0];
-                              ll = selElmnt.length;
-                              /*for each element, create a new DIV that will act as the selected item:*/
-                              a = document.createElement("DIV");
-                              a.setAttribute("class", "select-selected");
-                              a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
-                              x[i].appendChild(a);
-                              /*for each element, create a new DIV that will contain the option list:*/
-                              b = document.createElement("DIV");
-                              b.setAttribute("class", "select-items select-hide");
-                              for (j = 1; j < ll; j++) {
-                                /*for each option in the original select element,
-                                create a new DIV that will act as an option item:*/
-                                c = document.createElement("DIV");
-                                c.innerHTML = selElmnt.options[j].innerHTML;
-                                c.addEventListener("click", function(e) {
-                                    /*when an item is clicked, update the original select box,
-                                    and the selected item:*/
-                                    var y, i, k, s, h, sl, yl;
-                                    s = this.parentNode.parentNode.getElementsByTagName("select")[0];
-                                    sl = s.length;
-                                    h = this.parentNode.previousSibling;
-                                    for (i = 0; i < sl; i++) {
-                                      if (s.options[i].innerHTML == this.innerHTML) {
-                                        s.selectedIndex = i;
-                                        h.innerHTML = this.innerHTML;
-                                        y = this.parentNode.getElementsByClassName("same-as-selected");
-                                        yl = y.length;
-                                        for (k = 0; k < yl; k++) {
-                                          y[k].removeAttribute("class");
+                        <script>
+                              var x, i, j, l, ll, selElmnt, a, b, c;
+                                /*look for any elements with the class "custom-select":*/
+                              x = document.getElementsByClassName("custom-select");
+                                l = x.length;
+                                for (i = 0; i < l; i++) {
+                                selElmnt = x[i].getElementsByTagName("select")[0];
+                                ll = selElmnt.length;
+                                /*for each element, create a new DIV that will act as the selected item:*/
+                                a = document.createElement("DIV");
+                                a.setAttribute("class", "select-selected");
+                                a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
+                                x[i].appendChild(a);
+                                /*for each element, create a new DIV that will contain the option list:*/
+                                b = document.createElement("DIV");
+                                b.setAttribute("class", "select-items select-hide");
+                                for (j = 1; j < ll; j++) {
+                                  /*for each option in the original select element,
+                                  create a new DIV that will act as an option item:*/
+                                  c = document.createElement("DIV");
+                                  c.innerHTML = selElmnt.options[j].innerHTML;
+                                  c.addEventListener("click", function(e) {
+                                      /*when an item is clicked, update the original select box,
+                                      and the selected item:*/
+                                      var y, i, k, s, h, sl, yl;
+                                      s = this.parentNode.parentNode.getElementsByTagName("select")[0];
+                                      sl = s.length;
+                                      h = this.parentNode.previousSibling;
+                                      for (i = 0; i < sl; i++) {
+                                        if (s.options[i].innerHTML == this.innerHTML) {
+                                          s.selectedIndex = i;
+                                          h.innerHTML = this.innerHTML;
+                                          y = this.parentNode.getElementsByClassName("same-as-selected");
+                                          yl = y.length;
+                                          for (k = 0; k < yl; k++) {
+                                            y[k].removeAttribute("class");
+                                          }
+                                          this.setAttribute("class", "same-as-selected");
+                                          break;
                                         }
-                                        this.setAttribute("class", "same-as-selected");
-                                        break;
                                       }
-                                    }
-                                    h.click();
-                                });
-                                b.appendChild(c);
+                                      h.click();
+                                  });
+                                  b.appendChild(c);
+                                }
+                                x[i].appendChild(b);
+                                a.addEventListener("click", function(e) {
+                                    /*when the select box is clicked, close any other select boxes,
+                                    and open/close the current select box:*/
+                                    e.stopPropagation();
+                                    closeAllSelect(this);
+                                    this.nextSibling.classList.toggle("select-hide");
+                                    this.classList.toggle("select-arrow-active");
+                                  });
                               }
-                              x[i].appendChild(b);
-                              a.addEventListener("click", function(e) {
-                                  /*when the select box is clicked, close any other select boxes,
-                                  and open/close the current select box:*/
-                                  e.stopPropagation();
-                                  closeAllSelect(this);
-                                  this.nextSibling.classList.toggle("select-hide");
-                                  this.classList.toggle("select-arrow-active");
-                                });
-                            }
-                            function closeAllSelect(elmnt) {
-                              /*a function that will close all select boxes in the document,
-                              except the current select box:*/
-                              var x, y, i, xl, yl, arrNo = [];
-                              x = document.getElementsByClassName("select-items");
-                              y = document.getElementsByClassName("select-selected");
-                              xl = x.length;
-                              yl = y.length;
-                              for (i = 0; i < yl; i++) {
-                                if (elmnt == y[i]) {
-                                  arrNo.push(i)
-                                } else {
-                                  y[i].classList.remove("select-arrow-active");
+                              function closeAllSelect(elmnt) {
+                                /*a function that will close all select boxes in the document,
+                                except the current select box:*/
+                                var x, y, i, xl, yl, arrNo = [];
+                                x = document.getElementsByClassName("select-items");
+                                y = document.getElementsByClassName("select-selected");
+                                xl = x.length;
+                                yl = y.length;
+                                for (i = 0; i < yl; i++) {
+                                  if (elmnt == y[i]) {
+                                    arrNo.push(i)
+                                  } else {
+                                    y[i].classList.remove("select-arrow-active");
+                                  }
+                                }
+                                for (i = 0; i < xl; i++) {
+                                  if (arrNo.indexOf(i)) {
+                                    x[i].classList.add("select-hide");
+                                  }
                                 }
                               }
-                              for (i = 0; i < xl; i++) {
-                                if (arrNo.indexOf(i)) {
-                                  x[i].classList.add("select-hide");
-                                }
-                              }
-                            }
-                            /*if the user clicks anywhere outside the select box,
-                            then close all select boxes:*/
-                            document.addEventListener("click", closeAllSelect);
+                              /*if the user clicks anywhere outside the select box,
+                              then close all select boxes:*/
+                              document.addEventListener("click", closeAllSelect);
                           </script>
 
-</body>
                       </div> 
                       <div class="form-left">
                         <h3 class="form-text">Email Pengguna</h3>
